9cd0751ccb013fa809df8816c29c3ef6f1a1943d
edotienog <otieno1edwin@outlook.com>
2024-01-04 22:59:56 +0300

    update to leap year

---
+++ leapyears.py
@@ -0,0 +1,11 @@
+#Update to include leap year 
+num_years = 4
+days_per_year = 364.25 
+hours_per_day = 24
+mins_per_hour = 60
+secs_per_min = 60
+#Update 
+
+total_secs = secs_per_min* mins_per_hour * hours_per_day * days_per_year *num_years
+
+print (total_secs)
