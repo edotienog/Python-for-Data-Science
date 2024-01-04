3bd3efa7bc9442654968a4a791a4b4c1672c74fa
edotienog <otieno1edwin@outlook.com>
2024-01-04 22:53:24 +0300

    Multiple variable

---
+++ multvariable.py
@@ -0,0 +1,12 @@
+#Create variable 
+num_year = 4
+day_per_year = 365
+hours_per_day = 24
+mins_per_hour =60
+secs_per_min = 60
+
+#Calculate number of seconds in four years
+total_sec = secs_per_min* mins_per_hour * hours_per_day* day_per_year * num_year
+
+print (total_sec
+)
