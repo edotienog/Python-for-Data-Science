00cf4a9697de8b81c04d4dc966e8db36cf5601d8
edotienog <otieno1edwin@outlook.com>
2024-01-05 12:47:26 +0300

    Call a function

---
+++ function.py
@@ -0,0 +1,9 @@
+#Define the function
+def add_three(input_var):
+    output_var = input_var + 3
+    return output_var
+#Run the function with 10 as input
+new_number = add_three(10)
+
+    #Check the value is 13, as expected
+print(new_number)
