73ab037579d36f1ee4dffa6057ebb3b4d7f2533c
edotienog <otieno1edwin@outlook.com>
2024-01-10 13:09:21 +0300

    Method 2 Reverse list

---
+++ listreverse2.py
@@ -0,0 +1,5 @@
+
+# Method 2: Using the reversed() function
+my_list = [1, 2, 3] # Assign the list to a variable
+reversed_list = list(reversed(my_list)) # Create a reversed iterator and convert it to a list
+print(reversed_list) # Print the reversed list
