9e98b3eb87212b61917fdf9633e913f0e87b9e1d
edotienog <otieno1edwin@outlook.com>
2024-01-30 05:10:36 +0300

    List Data Structure

---
+++ flowerlist.py
@@ -0,0 +1,10 @@
+flowers_list =["pink primrose", "hard leaved pocket orchid", "canterbury bells", "sweat pea", "english marigold", "tiger lily", "moon orchird", "bird of paradise", "monkshood", "globe thistle"]
+#Check the type of list
+print(type(flowers_list))
+print(flowers_list)
+#Returns the length of the list
+print(len(flowers_list))
+print("First entry:", flowers_list[0])
+print("Second entry:", flowers_list[1])
+#The last entry is one less the count which is 9
+print("Last entry:", flowers_list[9])
