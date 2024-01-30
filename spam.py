6975c190ba2e1952a9aff0ea30c269cedfaa5996
edotienog <otieno1edwin@outlook.com>
2024-01-30 17:45:33 +0300

    Home to British troup Monty Python

---
+++ spam.py
@@ -0,0 +1,11 @@
+spam_amount = 0
+print(spam_amount)
+
+#Ordering spam, egg, Spam, spam, bacon and spam(4 more servings of spam)
+spam_amount = spam_amount + 4
+
+if spam_amount>0:
+    print("But I don't want Any spam!")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
