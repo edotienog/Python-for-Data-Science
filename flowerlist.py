a08b5168de6921f0094a4279268c2cf36c10a6ca
edotienog <otieno1edwin@outlook.com>
2024-01-30 05:20:23 +0300

    List methods

--- flowerlist.py
+++ flowerlist.py
@@ -8,3 +8,12 @@ print("First entry:", flowers_list[0])
 print("Second entry:", flowers_list[1])
 #The last entry is one less the count which is 9
 print("Last entry:", flowers_list[9])
+#Slicing 
+print("First 3 entry:", flowers_list[:3])
+print("Final 2 entries:", flowers_list[-2:])
+#Removing an item from a list with .remove(), then add the item in parathesis
+flowers_list.remove("globe thistle")
+print(flowers_list)
+#adding item we use .append() 
+flowers_list.append('snapdragon')
+print(flowers_list)
