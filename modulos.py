e087fd91cd5cafc3a4112e3ca8fa94c680f1c5e6
edotienog <otieno1edwin@outlook.com>
2024-01-31 06:54:13 +0300

    modulos = remaineder

---
+++ modulos.py
@@ -0,0 +1,6 @@
+james_bananas = 152 
+cameron_bananas = 83
+candy_bananas = 13
+#Modulos is % which is the remainder used here
+to_share = (james_bananas + cameron_bananas + candy_bananas) % 3
+print(to_share)
