534d0d5150f2bf77ffe60bd11b340a947c962595
edotienog <otieno1edwin@outlook.com>
2024-02-01 17:34:46 +0300

    Adding optional arguments with default value

---
+++ greet.py
@@ -0,0 +1,7 @@
+def greet(who="Colin"):
+    print("Hello,", who)
+
+greet()
+greet(who ="Edwin")
+#(In this case we don't need to specif the name of the argument base it's unambigous)
+greet("User")
