f3fe98f61dd3c12694cbdeba28cb9439eff03207
edotienog <otieno1edwin@outlook.com>
2024-02-05 06:24:56 +0300

    Using append method

--- list.py
+++ list.py
@@ -1,4 +1,5 @@
 planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-planets[-1]="pluto" #Changes the last element to pluto
+planets.append("pluto") #modify  a list by adding an item to the end
 print(sorted(planets)) #Sort the planets in alphabetical order
 print("There are", len(planets),"Planets") #List the number of element in a list
+help(planets.append)
