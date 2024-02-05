cd3fb2b00ffcd29d0d56fd0644a5bfa80ebb5913
edotienog <otieno1edwin@outlook.com>
2024-02-05 19:16:29 +0300

    In operator

--- sortlist.py
+++ sortlist.py
@@ -1,2 +1,4 @@
 list_animals = ["Eagle", "Cat", "Dogs", "Tiger", "Lion", "Cheetah", "Rabbit"]
-print(sorted(list_animals))
+# print(sorted(list_animals)) #Sort return the sorted list in alphabetical order
+print("Fox" in list_animals) #LIn operator to determine whether a list contain a particular value
+print("Dogs" in list_animals)
