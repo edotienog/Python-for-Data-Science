7da7eeff5f6c2703aa4695c1a3619522f445e297
edotienog <otieno1edwin@outlook.com>
2024-02-05 05:49:52 +0300

    List

---
+++ list.py
@@ -0,0 +1,4 @@
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+planets[-1]="pluto" #Changes the last element to pluto
+print(sorted(planets)) #Sort the planets in alphabetical order
+print("There are", len(planets),"Planets") #List the number of element in a list
