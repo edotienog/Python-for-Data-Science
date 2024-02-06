16eeebe4e001f52d6c2e8dcbe995e81a1b70a493
edotienog <otieno1edwin@outlook.com>
2024-02-06 05:44:44 +0300

    Select second element in LIST

---
+++ selectsecond.py
@@ -0,0 +1,9 @@
+def select_second(L):
+    """Returns the second element of the given list. If the el
+    the list has no second element, return none"""
+    if len(L)>= 2:
+        return L[1]
+    else: 
+        return None
+    
+print(select_second(["Kenya", "Ghana", "Sweden", "Poland", "Germany"]))
