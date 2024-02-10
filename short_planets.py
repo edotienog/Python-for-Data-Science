cfbeb5af846181a58a6e1886e57fbf54f3a8fa30
edotienog <otieno1edwin@outlook.com>
2024-02-10 05:21:59 +0300

    Using if conditions

---
+++ short_planets.py
@@ -0,0 +1,3 @@
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+short_planets = [planet for planet in planets if len(planet) < 6]# Add if condition to determine planets
+print(short_planets)
