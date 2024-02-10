2d08aeadf43cd4a43640888142f87f034524f755
edotienog <otieno1edwin@outlook.com>
2024-02-10 05:30:47 +0300

    If acting like WHERE in SQL- Filter

--- filter_planets.py
+++ filter_planets.py
@@ -1,4 +1,8 @@
 #str.upper() return all-caps version of a string
 planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-loud_short_planets = [planet.upper() + '!' for planet in planets if len(planet)<6] 
+loud_short_planets = [
+    planet.upper() + '!' 
+    for planet in planets 
+    if len(planet)<6
+] 
 print(loud_short_planets)
