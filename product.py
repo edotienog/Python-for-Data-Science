4246fb05372a597b4c9f9150801f61c9828c0f1d
edotienog <otieno1edwin@outlook.com>
2024-07-01 16:27:46 +0300

    Looping in tuples

--- product.py
+++ product.py
@@ -1,7 +1,9 @@
+#Tuples using brackets
 multiplicands = (2, 2, 2, 3, 3, 5)
 product = 1
+#Using for loop to loop over multiplicands
 for mult in multiplicands:
     product = product * mult
 product 
-
+#Printing the result of the product
 print(product)
